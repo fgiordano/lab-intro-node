f1b5dd51680645b09651424181b59ab68d3bf2e8
Francesca Giordano <francesca020406@hotmail.com>
2017-08-23 21:37:39 -0400

    1 more test has passed

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,23 @@
 var SortedList = function() {};
+var SortedList = function() {};
+
+SortedList.prototype.length = 0;
+SortedList.prototype.list = [];
+
+// var SortedList = function(){
+//   this.length = 0;
+//   this.list = [];
+// };
 
 SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
+SortedList.prototype.get     = function(pos) {
+  return this.list[pos-1]
+};
+
 SortedList.prototype.max     = function() {}
 SortedList.prototype.min     = function() {}
 SortedList.prototype.average = function() {}
 SortedList.prototype.sum     = function() {}
+
 
 module.exports = SortedList;
